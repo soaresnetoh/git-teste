46204cb19883363a53e2a90672d01ccab8afc7df
Hernani Soares <soaresnetoh@gmail.com>
2020-05-31 17:25:01 -0300

    Ainda em teste do Git

---
+++ aplicacao_02/aplicacao/rds.conf.php
@@ -0,0 +1,1 @@
+<?php $RDS_URL=''; $RDS_DB='dados'; $RDS_user='admin'; $RDS_pwd='Senha123456'; ?>
---
+++ aplicacao_02/aplicacao/s3-conf.php
@@ -0,0 +1,7 @@
+<?php
+$accessKey = 'Access key ID';
+$secretKey = 'Secret access key';
+$region = 'us-east-1';
+$bucket = 'NOME DO BUCKET';
+$arqName =  'logo.jpg';
+?>
